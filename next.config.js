ebf7c57a3a87684a91db2227fe7a30b53632c5ee
Elli <eleonora.nocentini.com>
2023-11-30 09:01:54 +0100

    first push

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images:{
+        remotePatterns:[
+            {
+                protocol: "https",
+                hostname:"ellis.0.capacedev.se/graphql",
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
